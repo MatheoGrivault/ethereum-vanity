003c586899e2c2f58e04f73ea42b62bf6693e8c9
kali <christophe.benedetti15@gmail.com>
2023-07-03 11:20:15 +0200

    command creation

--- src/cuda/main.cu
+++ src/cuda/main.cu
@@ -10,60 +10,56 @@
 #define THREADS_PER_BLOCK 256
 #endif
 
-enum class Command{
-    Account
-    Contract
+#include <iostream>
+#include <string>
+#include <cxxopts.hpp>
+
+enum class Command {
+    Account,
+    Contract,
     Help
 };
 
 struct Options {
     std::string prefix;
-    std::string suffix; 
+    std::string suffix;
     bool zeroBytes;
     bool ignoreCase;
-    int threads
+    int threads;
 };
 
-Command parseCommand(const std::string& commandstr){
-    if commandstr == "account"{
+Command parseCommand(const std::string& commandStr) {
+    if (commandStr == "account") {
         return Command::Account;
-    }
-
-    else if commandstr == "contract"{
+    } else if (commandStr == "contract") {
         return Command::Contract;
-    
-    }
-    else if commandstr == "help"{
+    } else if (commandStr == "help") {
         return Command::Help;
-    }
-
-    else{
-        std::cout<<"Invalid command"<<std::endl;
+    } else {
         return Command::Help;
     }
 }
 
-Options parseOptions(int argc, char* argv[]){
+Options parseOptions(int argc, char* argv[]) {
     Options options;
-    cxxopts::Options options("ethereum_vanity", "Bruteforce Ethereum addresses");
+
+    cxxopts::Options cmdlineOptions("ethereum_vanity", "Bruteforce Ethereum addresses");
 
     cmdlineOptions.add_options()
         ("p,prefix", "Address prefix", cxxopts::value<std::string>(options.prefix))
         ("s,suffix", "Address suffix", cxxopts::value<std::string>(options.suffix))
         ("z,zero-bytes", "Bruteforce forever until stopped by the user, keeping the address with the most zero bytes", cxxopts::value<bool>(options.zeroBytes))
         ("i,ignore-case", "Ignore case for prefix and suffix", cxxopts::value<bool>(options.ignoreCase))
-        ("t,threads", "Number of threads to use, or 0 to use all logical cores", cxxopts::value<int>(options.threads))
         ("h,help", "Print help")
         ("V,version", "Print version");
-    
-    cmdlineOptions.parse_positionnal({"command"});
+
+    cmdlineOptions.parse_positional({"command"});
 
     try {
-
         auto result = cmdlineOptions.parse(argc, argv);
 
-        if result.count("help"){
-            trow cxxopts::OptionException("Print help");
+        if (result.count("help")) {
+            throw cxxopts::OptionException("Print help");
         }
 
         if (result.count("version")) {
@@ -85,39 +81,30 @@ Options parseOptions(int argc, char* argv[]){
     }
 
     return options;
-
 }
 
 void printHelp(const cxxopts::Options& options) {
     std::cout << options.help() << std::endl;
 }
 
-int main(int argc, char ** argv[]){
-
+int main(int argc, char* argv[]) {
     Options options = parseOptions(argc, argv);
 
-    switch (options.command){
-
-        case command::Account:
-            /* code */
+    switch (options.command) {
+        case Command::Account:
+            // Code pour la commande 'account'
+            std::cout << "Bruteforce a private key" << std::endl;
+            // Ajoutez le code pour la commande 'account' ici
             break;
-        
-        case command::Contract:
-            /* code */
+        case Command::Contract:
+            // Code pour la commande 'contract'
+            std::cout << "Bruteforce a CREATE2 salt" << std::endl;
+            // Ajoutez le code pour la commande 'contract' ici
             break;
-        
-
         case Command::Help:
             printHelp(cmdlineOptions);
             break;
-    
     }
 
-
     return 0;
-    
-    
-
-    std::cout<<"Starting brute force attack"<<std::endl;
-
 }
